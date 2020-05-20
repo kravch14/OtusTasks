44909a4a918464d63a2c3e323d593d385808a732
kravchenko_andrei <kravch14@mail.ru>
2020-05-21 00:01:55 +0300

    Start creating the code for Yandex Market (now it is just a draft)

--- src/main/java/config/ServerConfig.java
+++ src/main/java/config/ServerConfig.java
@@ -5,7 +5,11 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:config.properties")
 public interface ServerConfig extends Config {
 
-    @Key("url")
-    @DefaultValue("http://ya.ru")
-    String url();
+    @Key("url1")
+    //@DefaultValue("http://ya.ru")
+    String url1();
+
+    @Key("url2")
+        //@DefaultValue("http://ya.ru")
+    String url2();
 }
--- src/test/java/TestHomeTask.java
+++ src/test/java/TestHomeTask.java
@@ -2,15 +2,17 @@ import config.ServerConfig;
 import factory.WebDriverFactory;
 import listeners.ExecutionListener;
 import org.aeonbits.owner.ConfigFactory;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-@Listeners(ExecutionListener.class)
+//@Listeners(ExecutionListener.class)
 public class TestHomeTask {
 
     public static final String BROWSER_CHROME = "chrome";
@@ -39,19 +41,53 @@ public class TestHomeTask {
             wd = WebDriverFactory.createNewDriver(browser, option);
         }
         logger.info(browser + CREATE_DRIVER_MESSAGE);
+        wd.manage().window().maximize();
+        logger.info("Размер страницы на весь экран установлен");
     }
+
+    /*@Test
+    public void openPageInBrowser() {
+        wd.get(cfg.url1());
+        logger.info(OPEN_PAGE_IN_BROWSER_MESSAGE);
+        /*wd.manage().addCookie(new Cookie("auth_token", "w01Pw5RHXt-vY0IiA97_0Q"));
+        wd.manage().addCookie(new Cookie("auth_token_expires", "1589230847"));
+        wd.manage().addCookie(new Cookie("carrotquest_session_started", "1"));
+    }*/
+public WebElement visible(WebDriver driver, String xPath) {
+    return new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath)));
+}
 
     @Test
-    public void openPageInBrowser() {
-        wd.get(cfg.url());
-        logger.info(OPEN_PAGE_IN_BROWSER_MESSAGE);
+    public void openYandexMarketPage() {
+        wd.get(cfg.url2());
+        logger.info("Открыта страница Яндекс Маркет");
+        WebDriverWait waitAllCategories = new WebDriverWait(wd, 30);
+
+        ((JavascriptExecutor) wd).executeScript("arguments[0].remove();", visible(wd, "//div[contains(@data-bem, 'market-searchFeature-onboarding')]"));
+        /*waitAllCategories.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='PXL2nleaah']")));
+
+            try {
+            WebElement pp = (new WebDriverWait(wd, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='popup2__content']"))));
+            while (pp.isDisplayed() && pp.isEnabled()) {
+                if (!(pp.isEnabled() && pp.isDisplayed())) {
+                    break;
+                }
+            }
+        } catch (NoSuchElementException e) {
+                System.out.println(e);
+        }
+        WebElement electronics = wd.findElement(By.cssSelector("a._3Lwc_UVFq4[href*=elektronika] > span"));
+        electronics.click();
+        WebElement mobilePhoneLink = wd.findElement(By.cssSelector("a[href*=catalog--mobilnye-telefony]"));
+        mobilePhoneLink.click();
+        //logger.info("Открыт раздел Все категории");*/
     }
 
-    @AfterTest
+    /*@AfterTest
     public void setDown() {
         if (wd != null) {
             wd.quit();
             logger.info(BROWSER_CLOSED_MESSAGE);
         }
-    }
+    }*/
 }
