c1503863577005ccdea37d4a6dc8b6ead383e84f
kravchenko_andrei <kravch14@mail.ru>
2020-05-28 22:39:23 +0300

    Add getProp

---
+++ src/main/java/properties/GetProperty.java
@@ -0,0 +1,10 @@
+package properties;
+
+import java.util.Optional;
+
+public class GetProperty {
+
+    public static Optional<String> getProp(String propertyName) {
+        return Optional.ofNullable(System.getenv(propertyName));
+    }
+}
